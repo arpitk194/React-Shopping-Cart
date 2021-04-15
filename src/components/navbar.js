76229c16534fcdccfe2a3b229e235f82a99f56ae
Arpit Kumar <arpitk194@gmail.com>
2021-04-15 21:18:31 +0530

    second changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ const NavBar = ({ totalCounters }) => {
   console.log("Navbar - Rendered");
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
